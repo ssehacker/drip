dc65699d858e58e0ead8ac782371bc61cd36c6f5
wb-zy198077 <wb-zy198077@alibaba-inc.com>
2016-06-23 00:21:28 +0800

    UI script links

--- app/router/router.js
+++ app/router/router.js
@@ -4,7 +4,9 @@ var router = require('koa-router')();
 var logger = require('log4js').getLogger('runtime');
 
 router.get('/', async (ctx, next) =>{
-	await ctx.render('index.jade');
+	await ctx.render('index.jade', {
+		host: 'http://127.0.0.1:3002'
+	});
 });
 
 
